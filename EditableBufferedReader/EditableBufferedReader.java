61be1a179b2f29501c0737dc6ac6799b9f33f7f2
Noa Paguera <noa.paguera@gmail.com>
2024-09-30 19:23:04 +0200

    Add setRaw and unsetRaw

--- EditableBufferedReader/EditableBufferedReader.java
+++ EditableBufferedReader/EditableBufferedReader.java
@@ -5,4 +5,22 @@ public class EditableBufferedReader extends BufferedReader {
     public EditableBufferedReader(InputStreamReader in) {
         super(in);
     }
+    
+    public void setRaw() {
+        try {
+            ProcessBuilder pb = new ProcessBuilder("stty -echo raw < /dev/tty");
+            pb.inheritIO().start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void unsetRaw() {
+        try {
+            ProcessBuilder pb = new ProcessBuilder("stty -echo cooked < /dev/tty");
+            pb.inheritIO().start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
