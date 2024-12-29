1d8e6624abac64812061e0def9c0ef51f1472a38
Noa Paguera <noa.paguera@gmail.com>
2024-12-30 00:45:18 +0100

    Add XatGrafic

---
+++ XatGrafic/MyServerSocket.java
@@ -0,0 +1,32 @@
+package XatGrafic;
+
+import java.io.IOException;
+import java.net.*;
+
+public class MyServerSocket {
+
+    private ServerSocket ss;
+
+    public MyServerSocket(int port) throws IOException {
+        this.ss = new ServerSocket(port);
+    }
+
+    public MySocket accept() {
+        try {
+            System.out.println("Socket acceptat");
+            return new MySocket(ss.accept());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void close() {
+        try {
+            ss.close();
+            System.out.println("S'ha tancat el servidor");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ XatGrafic/MySocket.java
@@ -0,0 +1,54 @@
+package XatGrafic;
+
+import java.io.*;
+import java.net.*;
+
+public class MySocket {
+    private Socket s;
+    private BufferedReader r;
+    private PrintWriter w;
+
+    public MySocket(String host, int port) {
+        try {
+            this.s = new Socket(host, port);
+            this.r = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            this.w = new PrintWriter(s.getOutputStream(), true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public MySocket(Socket socket) {
+        try {
+            this.s = socket;
+            this.r = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            this.w = new PrintWriter(s.getOutputStream(), true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String readLine() {
+        try {
+            return r.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void println(String msg) {
+        w.println(msg);
+    }
+
+    public void close() {
+        try {
+            if (r != null) r.close();
+            if (w != null) w.close();
+            s.close();
+            System.out.println("Socket tancat");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    } 
+}
